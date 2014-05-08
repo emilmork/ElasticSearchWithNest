aa3b7cb05c34029fe954a57739db2ae648119cbf
Emil Mork <emil.a.mork@gmail.com>
2014-05-08 09:15:12 +0200

    init commit

---
+++ ELApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ELApp.Startup))]
+namespace ELApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
